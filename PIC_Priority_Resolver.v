4dbf759d01f7eca040261580ebb5c6337627ad5c
Zainabbadr <zainabbadr32@gmail.com>
2024-01-02 22:21:30 +0200

    Helper functions to find the highest priority interrupt after rotation

--- PIC_Priority_Resolver.v
+++ PIC_Priority_Resolver.v
@@ -1,5 +1,6 @@
 module priority_resolver(
     input [2:0] priority_rotate,
+    input [7:0] interrupt_mask,
     input       special_fully_nested_config,
     input [7:0] highest_level_in_service,
 
@@ -11,6 +12,11 @@ module priority_resolver(
 
 );
 	
+	reg [7:0] masked_IRR ;
+	
+	always@* begin
+		masked_IRR = IRR & ~ interrupt_mask;
+	end
 	
 	reg [7:0] rotated_IRR;
     reg [7:0] rotated_ISR;
@@ -45,3 +51,56 @@ module priority_resolver(
 		interrupt = rotate_left(rotated_interrupt,priority_rotate);
 	end
 
+
+	/*---------------------------------------------------------------------	
+Helper functions to find the highest priority interrupt after rotation
+----------------------------------------------------------------------*/
+
+function [7:0] rotate_right(input [7:0] source, input [2:0] rotate);
+    case (rotate)
+		3'b000:  rotate_right = { source[0],   source[7:1] };
+        3'b001:  rotate_right = { source[1:0], source[7:2] };
+        3'b010:  rotate_right = { source[2:0], source[7:3] };
+        3'b011:  rotate_right = { source[3:0], source[7:4] };
+        3'b100:  rotate_right = { source[4:0], source[7:5] };
+        3'b101:  rotate_right = { source[5:0], source[7:6] };
+        3'b110:  rotate_right = { source[6:0], source[7]   };
+        3'b111:  rotate_right = source;
+        default: rotate_right = source;
+    endcase
+  endfunction
+  
+  
+  function [7:0] rotate_left(input [7:0] source, input [2:0] rotate);
+    case (rotate)
+         3'b000:  rotate_left = { source[6:0], source[7]   };
+         3'b001:  rotate_left = { source[5:0], source[7:6] };
+         3'b010:  rotate_left = { source[4:0], source[7:5] };
+         3'b011:  rotate_left = { source[3:0], source[7:4] };
+         3'b100:  rotate_left = { source[2:0], source[7:3] };
+         3'b101:  rotate_left = { source[1:0], source[7:2] };
+         3'b110:  rotate_left = { source[0],   source[7:1] };
+         3'b111:  rotate_left = source;
+         default: rotate_left = source;
+    endcase
+  endfunction
+  
+  
+  
+  function [7:0] resolv_priority(input [7:0] request);
+        if      (request[0] == 1'b1)    resolv_priority = 8'b00000001;
+        else if (request[1] == 1'b1)    resolv_priority = 8'b00000010;
+        else if (request[2] == 1'b1)    resolv_priority = 8'b00000100;
+        else if (request[3] == 1'b1)    resolv_priority = 8'b00001000;
+        else if (request[4] == 1'b1)    resolv_priority = 8'b00010000;
+        else if (request[5] == 1'b1)    resolv_priority = 8'b00100000;
+        else if (request[6] == 1'b1)    resolv_priority = 8'b01000000;
+        else if (request[7] == 1'b1)    resolv_priority = 8'b10000000;
+        else                            resolv_priority = 8'b00000000;
+  endfunction
+
+
+
+
+
+endmodule
